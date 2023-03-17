d7b0378ac883f7dfa4e8017bce521c223f45b782
wnx3 <106507628+wnx3@users.noreply.github.com>
2023-03-17 18:46:33 -0300

    Create CREATOR.py

--- CREATOR.py
+++ CREATOR.py
@@ -758,7 +758,7 @@ while cont is True:
         desired_caps['appActivity'] = 'com.facebook.lite.MainActivity'
         desired_caps['systemPort'] = random.randint(6000, 8299)
         desired_caps['noReset'] = True
-        desired_caps['app'] = 'InstagramLite.apk'
+        desired_caps['app'] = 'InstagramLite'
 
         driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
