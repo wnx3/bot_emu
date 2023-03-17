a3a61b42ecbf72df8b0ddb9d0f4f1428a8aabf49
wnx3 <106507628+wnx3@users.noreply.github.com>
2023-03-17 01:27:50 -0300

    Update CREATOR.py

--- CREATOR.py
+++ CREATOR.py
@@ -19,7 +19,6 @@ from mailtm import Email
 import re
 import logging
 logger = logging.getLogger(__name__)
-print('teste')
 handler = logging.FileHandler('log.txt')
 handler.setLevel(logging.ERROR)
 
